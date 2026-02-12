3bda1e126a08a7a2de5cc4426b1f5e951feb5bc7
Joxbard <83847909+Joxbard@users.noreply.github.com>
2026-02-12 11:01:40 -0600

    Update OpponentElementMixin.java
    Fixed Shiny battles

--- src/main/java/com/strangeone101/pixeltweaks/mixin/client/OpponentElementMixin.java
+++ src/main/java/com/strangeone101/pixeltweaks/mixin/client/OpponentElementMixin.java
@@ -28,105 +28,191 @@ import java.text.DecimalFormat;
 @Mixin(value = OpponentElement.class)
 public abstract class OpponentElementMixin extends PixelmonWidget {
 
-    @Shadow(remap = false)
-    private static ResourceLocation OPPONENT;
-    @Shadow(remap = false)
-    private static ResourceLocation HEALTHY;
-    @Shadow(remap = false)
-    private static ResourceLocation CAUTION;
-    @Shadow(remap = false)
-    private static ResourceLocation WARNING;
-    @Shadow(remap = false)
-    private static ResourceLocation SHINY;
-    @Shadow(remap = false)
-    private static ResourceLocation CAUGHT;
-    @Shadow(remap = false)
-    private static ResourceLocation TARGET;
-    @Shadow(remap = false)
-    private Screen parent;
-    @Shadow(remap = false)
-    private PixelmonClientData enemy;
-    @Shadow(remap = false)
-    private ScreenParticleEngine particleEngine;
+    @Shadow(remap = false) private static ResourceLocation OPPONENT;
+    @Shadow(remap = false) private static ResourceLocation HEALTHY;
+    @Shadow(remap = false) private static ResourceLocation CAUTION;
+    @Shadow(remap = false) private static ResourceLocation WARNING;
+    @Shadow(remap = false) private static ResourceLocation SHINY;
+    @Shadow(remap = false) private static ResourceLocation CAUGHT;
+    @Shadow(remap = false) private static ResourceLocation TARGET;
+
+    @Shadow(remap = false) private Screen parent;
+    @Shadow(remap = false) private PixelmonClientData enemy;
+    @Shadow(remap = false) private ScreenParticleEngine particleEngine;
 
     @Override
     public void drawElement(GuiGraphics graphics, float scale) {
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(770, 771);
-        ScreenHelper.drawImage(graphics, OPPONENT, (float)this.x, (float)(this.y - 3), 160.0F, 50.0F, this.zLevel);
-        float healthPercent = this.enemy.health.floatValue() / (float)this.enemy.maxHealth;
-        ScreenHelper.drawBar(graphics, (double)(this.x + 44), (double)(this.y + 20), 109.0D, 10.0D, healthPercent, this.enemy.getHealthColor(this.enemy.health.intValue(), this.enemy.maxHealth));
-        ScreenHelper.drawImage(healthPercent <= 0.5F ? (healthPercent <= 0.25F ? WARNING : CAUTION) : HEALTHY, graphics, (float)(this.x - 10), (float)(this.y - 18), 60.0F, 60.0F, this.zLevel);
+
+        ScreenHelper.drawImage(graphics, OPPONENT, (float) this.x, (float) (this.y - 3), 160.0F, 50.0F, this.zLevel);
+
+        float healthPercent = this.enemy.health.floatValue() / (float) this.enemy.maxHealth;
+        ScreenHelper.drawBar(
+                graphics,
+                (double) (this.x + 44),
+                (double) (this.y + 20),
+                109.0D,
+                10.0D,
+                healthPercent,
+                this.enemy.getHealthColor(this.enemy.health.intValue(), this.enemy.maxHealth)
+        );
+
+        ScreenHelper.drawImage(
+                healthPercent <= 0.5F ? (healthPercent <= 0.25F ? WARNING : CAUTION) : HEALTHY,
+                graphics,
+                (float) (this.x - 10),
+                (float) (this.y - 18),
+                60.0F,
+                60.0F,
+                this.zLevel
+        );
+
         float[] rgb = {1F, 1F, 1F};
         boolean boss = this.enemy.bossTier.isBoss();
         boolean shiny = this.enemy.palette.equalsIgnoreCase("shiny") && !boss;
 
         if (boss) {
-            //Dye the image based on this.enemy.bossTier.getColor()
             rgb = this.enemy.bossTier.getColor().getColorComponents(null);
         }
 
         ResourceLocation texture = ScreenHelper.getPokemonSprite(this.enemy, this.parent.getMinecraft());
-        Minecraft.getInstance().getTextureManager().bindForSetup(texture == null ? TextureManager.INTENTIONAL_MISSING_TEXTURE : texture);
-        PoseStack matrix = graphics.pose();
-        matrix.pushPose();
+        Minecraft.getInstance().getTextureManager().bindForSetup(
+                texture == null ? TextureManager.INTENTIONAL_MISSING_TEXTURE : texture
+        );
+
+        PoseStack pose = graphics.pose();
+        pose.pushPose();
+
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(770, 771);
+
         graphics.setColor(rgb[0], rgb[1], rgb[2], 1F);
+
         int ix = x + 1;
         int iy = y - 4;
-        matrix.translate((double)(x - (float)ix), (double)(y - (float)iy), (double)zLevel);
-        graphics.blit(texture, ix, iy, 0F, 0F, (int)40F, (int)40F, (int)40F, (int)40F);
-        graphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.enableBlend();
-        RenderSystem.blendFunc(770, 771);
-        matrix.popPose();
 
-        //ScreenHelper.drawImage(ScreenHelper.getPokemonSprite(this.enemy, this.parent.getMinecraft()), matrix, (float)(this.x + 1), (float)(this.y - 3), 40.0F, 40.0F, this.zLevel);
+        pose.translate((double) (x - ix), (double) (y - iy), (double) zLevel);
+        graphics.blit(texture, ix, iy, 0F, 0F, 40, 40, 40, 40);
+
         graphics.setColor(1F, 1F, 1F, 1F);
-        this.particleEngine.drawAtOffset(graphics, this.enemy.pokemonUUID.toString(), (double)(this.x + 5), (double)(this.y + 1), (double) RandomHelper.getRandom().nextInt(26), (double)RandomHelper.getRandom().nextInt(26));
+        pose.popPose();
+
+        graphics.setColor(1F, 1F, 1F, 1F);
+
+        this.particleEngine.drawAtOffset(
+                graphics,
+                this.enemy.pokemonUUID.toString(),
+                (double) (this.x + 5),
+                (double) (this.y + 1),
+                (double) RandomHelper.getRandom().nextInt(26),
+                (double) RandomHelper.getRandom().nextInt(26)
+        );
+
         float offset = 0.0F;
+
         if (ClientStorageManager.pokedex().hasCaught(this.enemy.toPokemonBase())) {
-            ScreenHelper.drawImage(graphics, CAUGHT, (float)(this.x + 52), (float)(this.y + 5), 8.0F, 8.0F, this.zLevel);
+            ScreenHelper.drawImage(graphics, CAUGHT, (float) (this.x + 52), (float) (this.y + 5), 8.0F, 8.0F, this.zLevel);
             offset += 9.0F;
         }
 
         if (this.enemy.getGender() != Gender.NONE) {
-            ScreenHelper.drawImage(this.enemy.getGender() == Gender.MALE ? Resources.male : Resources.female, graphics, (float)(this.x + 52) + offset, (float)(this.y + 5), 5.0F, 8.0F, this.zLevel);
+            ScreenHelper.drawImage(
+                    this.enemy.getGender() == Gender.MALE ? Resources.male : Resources.female,
+                    graphics,
+                    (float) (this.x + 52) + offset,
+                    (float) (this.y + 5),
+                    5.0F,
+                    8.0F,
+                    this.zLevel
+            );
             offset += 7.0F;
         }
 
         int labelColor = boss ? 0xff7c75 : (shiny ? -7545 : -986896);
-        ScreenHelper.drawScaledString(graphics, this.enemy.getDisplayName(), (float)(this.x + 52) + offset, (float)this.y + 5.75F, labelColor, 16.0F);
-        ScreenHelper.drawScaledStringRightAligned(graphics, "Lv." + this.enemy.level, (float)(this.x + 149), (float)this.y + 7.0F, -986896, false, 12.0F);
+
+        ScreenHelper.drawScaledString(
+                graphics,
+                this.enemy.getDisplayName(),
+                (float) (this.x + 52) + offset,
+                (float) this.y + 5.75F,
+                labelColor,
+                16.0F
+        );
+
+        ScreenHelper.drawScaledStringRightAligned(
+                graphics,
+                "Lv." + this.enemy.level,
+                (float) (this.x + 149),
+                (float) this.y + 7.0F,
+                -986896,
+                false,
+                12.0F
+        );
+
         if (PixelmonConfigProxy.getGraphics().isAdvancedBattleInformation()) {
             DecimalFormat df = new DecimalFormat(".#");
-            String percentage = df.format((double)healthPercent * 100.0D).replace(".0", "");
-            if (percentage.isEmpty()) {
-                percentage = "0";
-            }
+            String percentage = df.format(healthPercent * 100.0D).replace(".0", "");
+            if (percentage.isEmpty()) percentage = "0";
 
-            ScreenHelper.drawScaledStringRightAligned(graphics, percentage + "%", (float)(this.x + 145), (float)this.y + 22.0F, -986896, false, 14.0F);
+            ScreenHelper.drawScaledStringRightAligned(
+                    graphics,
+                    percentage + "%",
+                    (float) (this.x + 145),
+                    (float) this.y + 22.0F,
+                    -986896,
+                    false,
+                    14.0F
+            );
         }
 
         if (this.enemy.status != -1 && StatusType.getEffect(this.enemy.status) != null) {
-            float[] texturePair2 = StatusType.getTexturePos(StatusType.getEffect(this.enemy.status));
-            ScreenHelper.simpleDrawImageQuad(Resources.status, graphics, (float)(this.x + 54 + ScreenHelper.getStringWidth(this.enemy.getDisplayName())) + offset, (float)(this.y + 4), 10.5F, 10.5F, texturePair2[0] / 768.0F, texturePair2[1] / 768.0F, (texturePair2[0] + 240.0F) / 768.0F, (texturePair2[1] + 240.0F) / 768.0F, this.zLevel);
+            float[] texturePair = StatusType.getTexturePos(StatusType.getEffect(this.enemy.status));
+            ScreenHelper.simpleDrawImageQuad(
+                    Resources.status,
+                    graphics,
+                    (float) (this.x + 54 + ScreenHelper.getStringWidth(this.enemy.getDisplayName())) + offset,
+                    (float) (this.y + 4),
+                    10.5F,
+                    10.5F,
+                    texturePair[0] / 768.0F,
+                    texturePair[1] / 768.0F,
+                    (texturePair[0] + 240.0F) / 768.0F,
+                    (texturePair[1] + 240.0F) / 768.0F,
+                    this.zLevel
+            );
         }
 
         if (ClientProxy.battleManager.catchCombo != 0) {
-            ScreenHelper.drawScaledString(graphics, I18n.get("gui.battle.catch_combo", new Object[]{ClientProxy.battleManager.catchCombo}), (float)(this.x + 22) + offset, (float)this.y + 38.75F, -986896, 12.0F);
+            ScreenHelper.drawScaledString(
+                    graphics,
+                    I18n.get("gui.battle.catch_combo", ClientProxy.battleManager.catchCombo),
+                    (float) (this.x + 22) + offset,
+                    (float) this.y + 38.75F,
+                    -986896,
+                    12.0F
+            );
         }
 
         if (shiny && RandomHelper.getRandom().nextInt(80) == 0) {
             int size = 7 + RandomHelper.getRandom().nextInt(7);
-            this.particleEngine.addParticle(new ScreenParticleEngine.GuiParticle(this.enemy.pokemonUUID.toString(), SHINY, 0.0D, 0.0D, 1.0D, 0.0D, 0.0D, 0.0D, 1.0F, 0.8F, 0.3F, 0.0F, (float)size, (float)size, 120, (particle, matrixStack) -> {
-                int x = particle.age;
-                int m = particle.maxAge;
-                int h = m / 2;
-                particle.a = (float)(x <= h ? x : h - (x - h)) / (float)h;
-            }));
+            this.particleEngine.addParticle(
+                    new ScreenParticleEngine.GuiParticle(
+                            this.enemy.pokemonUUID.toString(),
+                            SHINY,
+                            0.0D, 0.0D, 1.0D,
+                            0.0D, 0.0D, 0.0D,
+                            1.0F, 0.8F, 0.3F, 0.0F,
+                            (float) size, (float) size,
+                            120,
+                            (particle, guiGraphics) -> {
+                                int age = particle.age;
+                                int max = particle.maxAge;
+                                int half = max / 2;
+                                particle.a = (float) (age <= half ? age : half - (age - half)) / (float) half;
+                            }
+                    )
+            );
         }
-
     }
 }
